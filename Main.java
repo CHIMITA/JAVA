f4afe775e0b9ffe1231ec626b21bafd6690a4fb9
JiWon <49803467+CHIMITA@users.noreply.github.com>
2021-03-29 15:54:18 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,53 @@
+package method2;
+
+public class Main {
+	
+	static int powerGage = 3;
+	
+	public static void main(String[] args) {
+		int userCommand = 1;
+		
+		if (powerGage > 1) {
+			attack(powerGage);
+		} else {
+			attack();
+		}
+		if (powerGage > 1) {
+			attack(powerGage);
+		} else {
+			attack();
+		}
+		if (powerGage > 1) {
+			attack(powerGage);
+		} else {
+			attack();
+		}
+		if (powerGage > 1) {
+			attack(powerGage);
+		} else {
+			attack();
+		}
+		if (powerGage > 1) {
+			attack(powerGage);
+		} else {
+			attack();
+		}
+		if (powerGage > 1) {
+			attack(powerGage);
+		} else {
+			attack();
+		}
+	
+	}
+
+	 private static void attack(int powerGage2) {
+		 --powerGage;
+		 System.out.println("½´ÆÛ¾ÆºÌ");
+		 System.out.println("PowerGage :" + powerGage);
+	}
+
+	static void attack() {
+		System.out.println("¾ÆºÌ");
+		
+	}
+}
